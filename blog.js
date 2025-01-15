54571487294c9dc0163567fd52010dbda0636bff
Rivanda-dev <ramdhanirivanda@gmail.com>
2025-01-15 09:41:19 +0700

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,21 @@
+const container = document.getElementsByClassName('container')[0];
+
+function generatebox(team){
+    const box = document.createElement('div');
+    box.className = 'box';
+    container.appendChild(box)
+    const img = document.createElement('img')
+    img.className = 'media'
+    img.src= team.image
+    box.appendChild(img)
+    const title = document.createElement('h4');
+    title.innerText = team.title
+    box.appendChild(title)
+    const path = document.createElement('p')
+    path.innerText = team.category
+    box.appendChild(path)
+}
+
+fetch ('https://fahram.dev/api/v2/posts')
+.then(response => response.json())
+.then(data => data.data.forEach(course => generatebox(course)))
